29c5ee98863e2dc094bcfccd9f3e5ac14705b68a
Jared Lewis <jared@jaredlewis.net>
2021-12-12 23:54:30 -0800

    day 6 cleanup

--- day-06/src/main/java/LaternfishSimulator.java
+++ day-06/src/main/java/LaternfishSimulator.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -13,31 +12,42 @@ import java.util.Map;
 public class LaternfishSimulator {
     private final List<Laternfish> school = new ArrayList<>();
 
-    public static void main(String[] args) throws IOException {
-        int[] exampleAges = parse("3,4,3,1,2");
+    public static void main(String[] args) {
+        try {
+            // Data
+            int[] exampleAges = parse("3,4,3,1,2");
+            InputStream inputStream = LaternfishSimulator.class.getClassLoader().getResourceAsStream("input.txt");
+            String input = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+            int[] ages = parse(input.trim());
 
-        LaternfishSimulator example = new LaternfishSimulator(exampleAges);
-        long exampleResult = example.simulate(80);
+            // Part one Example
+            LaternfishSimulator example = new LaternfishSimulator(exampleAges);
+            long exampleResult = example.simulate(80);
+            assertExample("Part one example", 5934, exampleResult);
 
-        if (exampleResult != 5934) {
-            throw new AssertionError(String.format("Example incorrect: expected %d got %d", 5934, exampleResult));
+            // Part one
+            LaternfishSimulator simulator = new LaternfishSimulator(ages);
+            long result = simulator.simulate(80);
+            System.out.println("Part one - total laternfish after 80 days: " + result);
+
+            // Part two example
+            long longExampleResult = example.simulate(256 - 80);
+            assertExample("Part two example", 26_984_457_539L, longExampleResult);
+
+            // Part two
+            long longResult = simulator.simulate(256 - 80);
+            System.out.println("Part two - total laternfish after 256 days: " + longResult);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
-        InputStream inputStream = LaternfishSimulator.class.getClassLoader().getResourceAsStream("input.txt");
-        String input = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-        int[] ages = parse(input.trim());
-        LaternfishSimulator simulator = new LaternfishSimulator(ages);
-        long result = simulator.simulate(80);
-
-        System.out.println("Part one - total laternfish after 80 days: " + result);
-
-        long longExampleResult = example.simulate(256 - 80);
-        if (longExampleResult != 26_984_457_539L) {
-            throw new AssertionError(String.format("Example incorrect: expected %d got %d", 26_984_457_539L, longExampleResult));
+    private static void assertExample(String name, long expected, long actual) {
+        if (actual != expected) {
+            throw new IllegalStateException(
+                String.format("%s incorrect. Expected: %d - Actual: %d", name, expected, actual)
+            );
         }
-
-        long longResult = simulator.simulate(256 - 80);
-        System.out.println("Part two - total laternfish after 256 days: " + longResult);
     }
 
     private static int[] parse(String text) {
